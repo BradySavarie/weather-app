e9aea7f2252487a388ce62d48b9266d0aedbedd6
Brady <brady.savarie@hotmail.com>
2023-04-14 20:30:39 -0400

    Create a currentLocation object

--- src/controller.js
+++ src/controller.js
@@ -1,4 +1,3 @@
-// The code below is the beginning of the model module. Still need to configure webpack for bundling before separating into modules. Just wanted to play around with fetching from the weather api :)
 import './styles.css';
 import * as Model from './model';
 import * as View from './view';
--- src/model.js
+++ src/model.js
@@ -2,15 +2,35 @@ const key = '6565137403ce4116af3205145230904';
 const baseUrl = 'http://api.weatherapi.com/v1';
 const forecastApiMethod = '/forecast.json';
 
-let currentLocation = 'Sudbury';
+class LocationData {
+    constructor(locationData) {
+        this.cityName = locationData.location.name;
+        this.currentCondition = locationData.current.condition.text;
+        this.currentTempCelcius = Math.round(locationData.current.temp_c);
+        this.currentTempFahrenheit = Math.round(locationData.current.temp_f);
+        this.highTempCelcius = Math.round(
+            locationData.forecast.forecastday[0].day.maxtemp_c
+        );
+        this.highTempFahrenheit = Math.round(
+            locationData.forecast.forecastday[0].day.maxtemp_f
+        );
+        this.lowTempCelcius = Math.round(
+            locationData.forecast.forecastday[0].day.mintemp_c
+        );
+        this.lowTempFahrenheit = Math.round(
+            locationData.forecast.forecastday[0].day.mintemp_f
+        );
+    }
+}
 
 export default async function getWeatherData(cityName) {
-    currentLocation = cityName;
     const responseData = await fetch(
-        `${baseUrl + forecastApiMethod}?key=${key}&q=${currentLocation}&days=7`
+        `${baseUrl + forecastApiMethod}?key=${key}&q=${cityName}&days=7`
     );
     const responseDataJson = await responseData.json();
     console.log(responseDataJson);
+    const currentLocation = new LocationData(responseDataJson);
+    console.log(currentLocation);
 }
 
 export { getWeatherData };
